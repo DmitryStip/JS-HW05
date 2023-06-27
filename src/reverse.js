cf82ef5f4804fdd60226798f4f81f65fa26331b8
Dmitry <rekssobaka@gmail.com>
2023-06-27 20:02:15 +0300

    AddReverse1

--- src/reverse.js
+++ src/reverse.js
@@ -11,8 +11,7 @@ Array.prototype.reversePrototype = function() {
     }
   };
   
-  // Пример использования
   const arr = [1, 2, 3, 4, 5];
   arr.reversePrototype();
-  console.log(arr); // [5, 4, 3, 2, 1]
+  console.log(arr);
   
